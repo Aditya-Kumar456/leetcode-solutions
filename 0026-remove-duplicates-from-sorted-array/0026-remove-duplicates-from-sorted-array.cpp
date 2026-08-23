6c1990181547aefb999fcf7456071f340afd1553
Aditya Kumar <kumaraditya2124@gmail.com>
2026-08-23 20:17:20 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,9 +4,7 @@ public:
         int i = 0;
         for(int j = 1; j < nums.size(); j++){
             if(nums[i] < nums[j]){
-                int temp = nums[i + 1];
-                nums[i + 1] = nums[j];
-                nums[j] = temp;
+                swap(nums[i + 1], nums[j]);
                 i++;
             }
         }
