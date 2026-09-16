982792e6dd100591f21f1390307e4802ff462563
Aditya Kumar <kumaraditya2124@gmail.com>
2026-09-16 10:30:45 +0530

    Time: 67 ms (30.7%), Space: 152.2 MB (21.32%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,32 +1,50 @@
 class MinStack {
 public:
-    vector<int> arr;
-    vector<int> minArr;
-
+    int *arr;
+    int *minArr;
+    int size;
+    int topIndex;
     MinStack() {
+        size = 30000;
+        topIndex = -1;
+        arr = new int[size];
+        minArr = new int[size];
     }
-
+    
     void push(int value) {
-        arr.push_back(value);
+        if(topIndex < size - 1){
+            topIndex++;
+            arr[topIndex] = value;
 
-        if (minArr.empty()) {
-            minArr.push_back(value);
-        }
-        else {
-            minArr.push_back(min(value, minArr.back()));
+            if(topIndex == 0){
+                minArr[topIndex] = value;
+            }
+            else {
+                minArr[topIndex] = min(value, minArr[topIndex - 1]);
+            }
         }
     }
-
+    
     void pop() {
-        arr.pop_back();
-        minArr.pop_back();
+        if(topIndex >= 0){
+            topIndex--;
+        }
     }
-
+    
     int top() {
-        return arr.back();
+        return arr[topIndex];
     }
-
+    
     int getMin() {
-        return minArr.back();
+        return minArr[topIndex];
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
