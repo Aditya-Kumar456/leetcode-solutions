27ee79c1f8d3b65300ac1df536b3c0d3ed053495
Aditya Kumar <kumaraditya2124@gmail.com>
2026-09-18 18:08:43 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
     
-    int solve(int n, vector<int>&dp){
-        if(n <= 1){
-            return n;
-        }
+    // int solve(int n, vector<int>&dp){
+    //     if(n <= 1){
+    //         return n;
+    //     }
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
 
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-        return dp[n];
-    }
+    //     dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    //     return dp[n];
+    // }
 
-    int solve1(int n, vector<int>&dp){
-        if(n <= 1){
-            return n;
-        }
+    // int solve1(int n, vector<int>&dp){
+    //     if(n <= 1){
+    //         return n;
+    //     }
 
-        dp[0] = 0;
-        dp[1] = 1;
+    //     dp[0] = 0;
+    //     dp[1] = 1;
 
-        for(int i = 2; i <= n; i++){
-        dp[i] = dp[i - 1] + dp[i - 2];
-        }
+    //     for(int i = 2; i <= n; i++){
+    //     dp[i] = dp[i - 1] + dp[i - 2];
+    //     }
 
-        return dp[n];
-    }
+    //     return dp[n];
+    // }
 
     int fib(int n) {
-        vector<int>dp(n + 1, -1);
-        return solve1(n, dp);
+        // vector<int>dp(n + 1, -1);
+        // return solve1(n, dp);
+
+        if(n <= 1){
+            return n;
+        }
+
+        int prev2 = 0;
+        int prev1 = 1;
+        for(int i = 2; i <= n; i++){
+            int curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
