d553ce867fce6954020d9028b483ecdac540dd5d
Aditya Kumar <kumaraditya2124@gmail.com>
2026-09-18 18:04:50 +0530

    Time: 1 ms (77.53%), Space: 8.2 MB (17.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -14,8 +14,23 @@ public:
         return dp[n];
     }
 
+    int solve1(int n, vector<int>&dp){
+        if(n <= 1){
+            return n;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+        dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     int fib(int n) {
         vector<int>dp(n + 1, -1);
-        return solve(n, dp);
+        return solve1(n, dp);
     }
 };
